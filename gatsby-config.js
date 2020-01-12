32f70adca457e0c7efc62342a5a410791dea2914
Devansh Purohit <devanshpurohit@Devanshs-MacBook-Pro.local>
2020-01-12 20:32:24 +0530

    Remove extra clutter and add personal info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Blogged`,
+    name: `Blogged`,
+    siteUrl: `https://blogged.netlify.com`,
+    description: `A collaborative between Devansh and Chahak. Posts on React, JS, Git, Vim, Design Systems and sometimes Random.`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
+      heading: `Welcome to Blogged.`,
+      maxWidth: 652
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
+        url: `https://twitter.com/narative`
+      }
+    ]
   },
   plugins: [
     {
@@ -40,10 +24,10 @@ module.exports = {
         basePath: "/",
         authorsPage: true,
         sources: {
-          local: true,
+          local: true
           // contentful: true,
-        },
-      },
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -54,8 +38,8 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
-    },
-  ],
+        icon: `src/assets/favicon.png`
+      }
+    }
+  ]
 };
